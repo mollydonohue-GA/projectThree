3308442762070f35ff1a169f3507e9d63eb65b38
Joshua Brain Jaffe-Pinckney <joshuabrainjaffe@gmail.com>
2015-11-23 17:11:50 -0500

    added user model

---
+++ lunchapp/models/user.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose');
+
+var userSchema = mongoose.Schema({
+
+	first_name: String,
+  last_name: String,
+  email: String,
+	password: String
+
+});
+
+var User = mongoose.model('user', userSchema);
+module.exports = User;
