de713ca5e4d9a80e2d075879f9f5529276de7937
Joshua Brain Jaffe-Pinckney <joshuabrainjaffe@gmail.com>
2015-11-23 18:51:51 -0500

    init commit - added user login (haven't tested yet)

--- lunchapp/public/app.js
+++ lunchapp/public/app.js
@@ -1,1 +1,139 @@
 console.log("let us lunch.");
+
+$(function() {
+
+	//make the buttons clicky
+	$('#signup-button').click(function(){ renderNewUserForm() });
+
+	$('#login-button').click(function(){ renderUserForm() });
+
+	$('#logout').click(function(){ Cookies.remove('loggedinId') });
+//////////////////////////////////////////////////////////////////
+////////////////////SIGN UP///////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+
+	//render the sign up form when the signup button is clicked
+	var renderNewUserForm = function(){
+
+		var $formDiv = $('#login-div');
+
+		$formDiv.show();
+		$('.form').empty();
+		$('#login-button').show();
+		$('#signup-button').hide();
+		var template = Handlebars.compile($('#user-form').html());
+
+		$formDiv.append(template);
+
+		$('#user-submit').click(function() {
+			console.log('make this new user a thing!');
+			$('#login-button').hide();
+			createUser();
+		})
+		.html("sign up");
+
+	};
+
+	// creates new user via POST route
+	var createUser = function(){
+
+		console.log('Make a new user!');
+
+		var first_name = $('#first_name-input').val();
+		var last_name = $('#last_name-input').val();
+    var email = $('#email-input').val();
+		var password = $('#password-input').val();
+
+		var user = {
+      first_name: first_name,
+      last_name: last_name,
+			email: email,
+			password: password
+		}
+		$.post('/users', user)
+			.done(newUser);
+
+	};
+
+	//welcome a new user after creation
+	var newUser = function(data){
+
+		$('#login-div').hide();
+
+		var template = Handlebars.compile($('#template').html());
+
+		$('#MAIN CONTAINER').append(template(data))
+
+		//get dat cookie
+		user = Cookies.get();
+
+	};
+
+//////////////////////////////////////////////////////////////////
+////////////////////LOG IN////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+
+	//render the log in form when the log in button is clicked
+	var renderUserForm = function(){
+
+		console.log("app.js clicked log in");
+
+		var $formDiv = $('#login-div');
+
+		$formDiv.show();
+		$('.form').empty();
+		$('#login-button').hide();
+		$('#signup-button').show();
+
+		var template = Handlebars.compile($('#user-form').html());
+		$formDiv.append(template);
+
+		$('#user-submit').click(function() {
+			console.log('app.js logn user in');
+			loginUser();
+			})
+			.html("login");
+
+	};
+
+	// log in via POST route
+	var loginUser = function(){
+
+		console.log('app.js welcome user');
+
+    var first_name = $('#first_name-input').val();
+		var last_name = $('#last_name-input').val();
+    var email = $('#email-input').val();
+		var password = $('#password-input').val();
+
+    var user = {
+      first_name: first_name,
+      last_name: last_name,
+			email: email,
+			password: password
+		}
+		$.post('/login', user)
+			.done(function(data){
+				$('#signup-button').hide();
+				$('#login-div').hide();
+			})
+			.fail(function(){
+				alert("app.js login failed " + user.username)
+			})
+	};
+
+	//welcome a user after logging in
+	var returningUser = function(data){
+
+		//get dat cookie
+		user = getCookie();
+
+		$('#login-div').hide();
+
+		var template = Handlebars.compile($('#template').html());
+
+		$('#users').append(template(data))
+
+	}
+
+}); //end of everything
---
+++ lunchapp/server.js
@@ -0,0 +1,111 @@
+// DEPENDENCIES
+var express      = require('express'),
+    mongoose     = require('mongoose'),
+    bodyParser   = require('body-parser'),
+    md5          = require('md5'),
+    cookieParser = require('cookie-parser');
+
+var port         = process.env.PORT || 3000;
+var app          = express();
+
+// MIDDLEWARE
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(express.static('public'));
+
+app.use(cookieParser());
+
+// DATABASE
+mongoose.connect('mongodb://localhost/feels');
+
+// LISTENER
+app.listen(port);
+
+// MODELS
+var User = require('./models/user')
+var Restaurant = require('./models/restaurant')
+
+///////////////////////////////////////
+// USER ROUTES ////////////////////////
+///////////////////////////////////////
+
+// SIGN UP ( CREATE NEW USER )
+app.post('/users', function(req, res) {
+
+  password_hash = md5(req.body.password);
+
+	var user = new User({
+    first_name: req.body.first_name,
+    last_name: req.body.last_name,
+    email: req.body.email,
+    password: password_hash
+  });
+
+  user.save(function(err) {
+		if(err) {
+			console.log(err);
+		} else {
+
+      res.cookie("loggedinId", user.id)
+
+			res.send({
+        id: user.id,
+        username: user.username,
+
+      });
+		};
+	});
+});
+
+
+//LOG IN OLD USER
+app.post('/login', function(req, res) {
+  console.log("got past 500 error");
+
+  var first_name = req.body.first_name;
+  var last_name = req.body.last_name;
+  var email = req.body.email;
+  var password = req.body.password;
+
+  //MAYBE SWITCH TO USER ID!?!?!?!?!
+  User.findOne({ 'email': email }, function( err, user){
+
+    var request_password_hash = md5( password );
+
+    if( user != null && request_password_hash == user.password){
+
+        res.cookie("loggedinId", user.id)
+        res.send("server.js - logged in")
+
+    } else {
+
+      res.status = 403;
+      res.send("server.js - didn't login")
+
+    }
+
+  });
+
+});
+
+//LOG OUT
+
+///////////////////////////////////////
+// RESTAURANT ROUTES //////////////////
+///////////////////////////////////////
+
+// GET ROUTE
+app.get('/restaurants', function(req, res){
+
+  if (req.cookies.loggedinId != undefined){
+
+    res.send("STUFF")
+
+  } else {
+
+    res.send("NO STUFF FOR YOU")
+
+  }
+
+});
