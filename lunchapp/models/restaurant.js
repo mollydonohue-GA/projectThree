9292309669451f7cdc82459ed85c68ca6af116ee
Joshua Brain Jaffe-Pinckney <joshuabrainjaffe@gmail.com>
2015-11-25 13:59:06 -0500

    init commit

---
+++ lunchapp/models/restaurant.js
@@ -0,0 +1,24 @@
+var mongoose = require('mongoose');
+var ComplaintSchema = require('./complaint').schema;
+
+var RestaurantSchema = new mongoose.Schema({
+
+	name: String,
+	foodType: String,
+	phone: Number,
+	urlRestaurant: String,
+	urlMenu: String,
+	urlOnline: String,
+	crossStreet: String,
+	position: {
+		lat: Number,
+		lng: Number
+		}
+	orderOnline: boolean,
+	seating: boolean,
+	whosGoing: Array
+
+});
+
+var Restaurant = mongoose.model('Restaurant', RestaurantSchema);
+module.exports = Restaurant;
--- lunchapp/public/app.js
+++ lunchapp/public/app.js
@@ -142,4 +142,3 @@ $(function() {
 	}
 
 }); //end of everything
-
